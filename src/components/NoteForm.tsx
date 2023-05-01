8b10a0f94036bef65cf389aa9320f1e20d53f171
Taliah Wharton <taliahwh23@yahoo.com>
2023-05-01 16:27:23 -0400

    create new note + persist in local storage

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -2,15 +2,18 @@ import React, { useRef, useState } from 'react';
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CreateableReactSelect from 'react-select/creatable';
+import { v4 as uuidV4 } from 'uuid';
 
 // Types
 import { NoteData, Tag } from '../App';
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-function NoteForm({ onSubmit }: NoteFormProps) {
+function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
@@ -18,10 +21,13 @@ function NoteForm({ onSubmit }: NoteFormProps) {
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
 
+    const currentDate = new Date().toJSON();
+
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: [],
+      tags: selectedTags,
+      date: currentDate,
     });
   }
   return (
@@ -36,7 +42,27 @@ function NoteForm({ onSubmit }: NoteFormProps) {
           <Col>
             <Form.Group controlId="tags"></Form.Group>
             <Form.Label>Tags</Form.Label>
-            <CreateableReactSelect isMulti />
+            <CreateableReactSelect
+              isMulti
+              options={availableTags.map((tag) => {
+                return { label: tag.label, value: tag.id };
+              })}
+              onCreateOption={(label) => {
+                const newTag = { id: uuidV4(), label };
+                onAddTag(newTag);
+                setSelectedTags((prev) => [...prev, newTag]);
+              }}
+              onChange={(tags) => {
+                setSelectedTags(
+                  tags.map((tag) => {
+                    return { label: tag.label, id: tag.value };
+                  })
+                );
+              }}
+              value={selectedTags.map((tag) => {
+                return { label: tag.label, value: tag.id };
+              })}
+            />
           </Col>
         </Row>
         <Form.Group controlId="markdown"></Form.Group>
