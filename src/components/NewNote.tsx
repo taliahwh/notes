b867a0f9287b4c17ff749e49ea447b43e247cbec
Taliah Wharton <taliahwh23@yahoo.com>
2023-05-01 16:27:18 -0400

    create new note + persist in local storage

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import NoteForm from './NoteForm';
 
-function NewNote() {
+// Types
+import { NoteData, Tag } from '../App';
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
       <h1 className="mb-4">NewNote</h1>
-      <NoteForm />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 }
