2f8dd3335d74ccc5ff99fe49a59eb15769dcd074
Taliah Wharton <taliahwh23@yahoo.com>
2023-05-02 16:17:29 -0400

    edit, update, delete notes/tags

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import CreateableReactSelect from 'react-select/creatable';
 import { v4 as uuidV4 } from 'uuid';
 
@@ -11,12 +11,20 @@ type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
-function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+function NoteForm({
+  title = '',
+  markdown = '',
+  tags = [],
+  onSubmit,
+  onAddTag,
+  availableTags,
+}: NoteFormProps) {
+  const navigate = useNavigate();
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
@@ -29,6 +37,8 @@ function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
       tags: selectedTags,
       date: currentDate,
     });
+
+    navigate('/');
   }
   return (
     <Form onSubmit={handleSubmit}>
@@ -37,37 +47,44 @@ function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
           <Col>
             <Form.Group controlId="title"></Form.Group>
             <Form.Label>Title</Form.Label>
-            <Form.Control required ref={titleRef} />
+            <Form.Control required ref={titleRef} defaultValue={title} />
           </Col>
           <Col>
-            <Form.Group controlId="tags"></Form.Group>
-            <Form.Label>Tags</Form.Label>
-            <CreateableReactSelect
-              isMulti
-              options={availableTags.map((tag) => {
-                return { label: tag.label, value: tag.id };
-              })}
-              onCreateOption={(label) => {
-                const newTag = { id: uuidV4(), label };
-                onAddTag(newTag);
-                setSelectedTags((prev) => [...prev, newTag]);
-              }}
-              onChange={(tags) => {
-                setSelectedTags(
-                  tags.map((tag) => {
-                    return { label: tag.label, id: tag.value };
-                  })
-                );
-              }}
-              value={selectedTags.map((tag) => {
-                return { label: tag.label, value: tag.id };
-              })}
-            />
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreateableReactSelect
+                isMulti
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+              />
+            </Form.Group>
           </Col>
         </Row>
         <Form.Group controlId="markdown"></Form.Group>
         <Form.Label>Body</Form.Label>
-        <Form.Control required as="textarea" rows={15} ref={markdownRef} />
+        <Form.Control
+          required
+          as="textarea"
+          rows={15}
+          ref={markdownRef}
+          defaultValue={markdown}
+        />
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
             Save
